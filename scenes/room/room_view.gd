070c1d43ca66f6c2a737a0a2ee033af28a34aab2
semallush <semallush@gmail.com>
2025-07-13 21:16:14 +0200

    lol

--- scenes/room/room_view.gd
+++ scenes/room/room_view.gd
@@ -16,7 +16,7 @@ var room_size = Vector2(1,1)
 @export var reload = false :
 	set(new_reload):
 		reload = false
-		update_room()
+		#update_room()
 		update_doors()
 		update_camera()
 		
@@ -25,45 +25,45 @@ static func new_room(room_size:Vector2):
 	new_room.room_size = room_size
 	
 func _ready() -> void:
-	update_room()
+	#update_room()
 	update_doors()
 	update_camera()
 	spawn_door()
 
-func update_room() -> void:
-	
-	$floor.mesh = PlaneMesh.new()
-	$floor.mesh.size = Vector2(room_width, room_depth)
-	
-	$ceiling.mesh = PlaneMesh.new()
-	$ceiling.mesh.size = Vector2(room_width, room_depth)
-	$ceiling.rotation = Vector3(0,0,PI)
-	$ceiling.position = Vector3(0,room_height,0)
-	
-	$wall_left.mesh = PlaneMesh.new()
-	$wall_left.mesh.size = Vector2(room_height, room_depth)
-	$wall_left.position = Vector3(-room_width/2, room_height/2, 0)
-	$wall_left.rotation = Vector3(0,0, -PI/2)
-	
-	$wall_right.mesh = PlaneMesh.new()
-	$wall_right.mesh.size = Vector2(room_height, room_depth)
-	$wall_right.position = Vector3(room_width/2, room_height/2, 0)
-	$wall_right.rotation = Vector3(0,0, PI/2)
-
-	$wall_back.mesh = PlaneMesh.new()
-	$wall_back.mesh.size = Vector2(room_width, room_height) 
-	$wall_back.position = Vector3(0, room_height/2, -room_depth/2)
-	$wall_back.rotation = Vector3(PI/2,0,0)
-	
-	$black_bar_left.mesh = PlaneMesh.new()
-	$black_bar_left.mesh.size = Vector2(room_width, room_height) 
-	$black_bar_left.position = Vector3(-room_width, room_height/2, room_depth/2)
-	$black_bar_left.rotation = Vector3(PI/2,0,0)
-	
-	$black_bar_right.mesh = PlaneMesh.new()
-	$black_bar_right.mesh.size = Vector2(room_width, room_height) 
-	$black_bar_right.position = Vector3(room_width, room_height/2, room_depth/2)
-	$black_bar_right.rotation = Vector3(PI/2,0,0)
+#func update_room() -> void:
+	#
+	#$floor.mesh = PlaneMesh.new()
+	#$floor.mesh.size = Vector2(room_width, room_depth)
+	#
+	#$ceiling.mesh = PlaneMesh.new()
+	#$ceiling.mesh.size = Vector2(room_width, room_depth)
+	#$ceiling.rotation = Vector3(0,0,PI)
+	#$ceiling.position = Vector3(0,room_height,0)
+	#
+	#$wall_left.mesh = PlaneMesh.new()
+	#$wall_left.mesh.size = Vector2(room_height, room_depth)
+	#$wall_left.position = Vector3(-room_width/2, room_height/2, 0)
+	#$wall_left.rotation = Vector3(0,0, -PI/2)
+	#
+	#$wall_right.mesh = PlaneMesh.new()
+	#$wall_right.mesh.size = Vector2(room_height, room_depth)
+	#$wall_right.position = Vector3(room_width/2, room_height/2, 0)
+	#$wall_right.rotation = Vector3(0,0, PI/2)
+#
+	#$wall_back.mesh = PlaneMesh.new()
+	#$wall_back.mesh.size = Vector2(room_width, room_height) 
+	#$wall_back.position = Vector3(0, room_height/2, -room_depth/2)
+	#$wall_back.rotation = Vector3(PI/2,0,0)
+	#
+	#$black_bar_left.mesh = PlaneMesh.new()
+	#$black_bar_left.mesh.size = Vector2(room_width, room_height) 
+	#$black_bar_left.position = Vector3(-room_width, room_height/2, room_depth/2)
+	#$black_bar_left.rotation = Vector3(PI/2,0,0)
+	#
+	#$black_bar_right.mesh = PlaneMesh.new()
+	#$black_bar_right.mesh.size = Vector2(room_width, room_height) 
+	#$black_bar_right.position = Vector3(room_width, room_height/2, room_depth/2)
+	#$black_bar_right.rotation = Vector3(PI/2,0,0)
 func update_doors() -> void:
 	# add size and rotation in here as well so it's all in code
 	pass
@@ -77,8 +77,9 @@ func update_doors() -> void:
 
 		
 func update_camera() -> void:
+	pass
 	# maybe this is ugly. idk what the best node hierarchy is
-	get_node("camera").position = Vector3(0, room_height/2, room_size.y/2+1)
+	#get_node("camera").position = Vector3(0, room_height/2, room_size.y/2+1)
 
 func spawn_door() -> void:
 	var new_door = DOOR_SCENE.instantiate()
