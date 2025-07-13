8977e0b611bf39fb5ee41bfdd237b9da982e4300
semallush <semallush@gmail.com>
2025-07-13 15:19:14 +0200

    added doors

--- scenes/room/room_object.gd
+++ scenes/room/room_object.gd
@@ -5,17 +5,24 @@ extends Node3D
 @export var room_width: float = 1
 @export var room_depth: float = 1
 @export var room_height: float = 2
+@export var show_door_left = false
+@export var show_door_right = false
+@export var show_door_back = false
+
 @export var reload = false :
 	set(new_reload):
 		reload = false
 		update_room()
+		update_doors()
 		update_camera()
 
 func _ready() -> void:
 	update_room()
+	update_doors()
 	update_camera()
 
 func update_room() -> void:
+	
 	$floor.mesh = PlaneMesh.new()
 	$floor.mesh.size = Vector2(room_width, room_depth)
 	
@@ -48,7 +55,26 @@ func update_room() -> void:
 	$black_bar_right.mesh.size = Vector2(room_width, room_height) 
 	$black_bar_right.position = Vector3(room_width, room_height/2, room_depth/2)
 	$black_bar_right.rotation = Vector3(PI/2,0,0)
+func update_doors() -> void:
+	# add size and rotation in here as well so it's all in code
+	if show_door_left:
+		$door_left.visible = true
+		$door_left.position = Vector3(-room_width/2, 0, 0)
+	else:
+		$door_left.visible = false
+		
+	if show_door_right:
+		$door_right.visible = true
+		$door_right.position = Vector3(room_width/2, 0, 0)
+	else:
+		$door_right.visible = false
 	
+	if show_door_back:
+		$door_back.visible = true
+		$door_back.position = Vector3(0, 0, -room_depth/2)
+	else:
+		$door_back.visible = false
+		
 func update_camera() -> void:
 	# maybe this is ugly. idk what the best node hierarchy is
 	get_parent().get_node("camera").position = Vector3(0, room_height/2, room_depth/2+1)
